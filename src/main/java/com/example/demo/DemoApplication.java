adf77bfa2928061afcf866c4b5d57b0685e4b5fa
王于晟 <brande.wang@hotmail.com>
2025-12-23 10:35:41 +0800

    hello world

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController  // 添加这个注解
+
 public class DemoApplication {
 
+    // 添加一个简单的 REST 端点
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
